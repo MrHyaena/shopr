5f807c5c2dcb1d085cc9b0785599bb14943f2934
MrHyaena <dolezalmartin131@gmail.com>
2025-03-24 18:36:46 +0100

    BlackDivider deleting bg image

--- website/app/components/BlackDivider.jsx
+++ website/app/components/BlackDivider.jsx
@@ -3,15 +3,7 @@ import React from "react";
 export function BlackDivider({ children }) {
   return (
     <>
-      <div
-        style={{
-          background: "url('./backgroundImg.webp')",
-          boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.9)",
-          backgroundSize: "cover",
-          backgroundPosition: "0px",
-        }}
-        className=" flex flex-col items-center justify-center py-15"
-      >
+      <div className=" flex flex-col items-center justify-center py-15 bg-primary">
         <div className="max-w-wrapper w-full">{children}</div>
       </div>
     </>
