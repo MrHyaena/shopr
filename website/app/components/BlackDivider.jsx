591efeed1d79517501c21a6eec39e01984226821
MrHyaena <dolezalmartin131@gmail.com>
2025-04-24 15:16:03 +0200

    Text

--- website/app/components/BlackDivider.jsx
+++ website/app/components/BlackDivider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function BlackDivider({ children }) {
   return (
     <>
-      <div className=" flex flex-col items-center justify-center py-15 bg-primary font-primary">
+      <div className=" flex flex-col items-center justify-center py-15 bg-primary font-primary px-3">
         <div className="max-w-wrapper w-full">{children}</div>
       </div>
     </>
